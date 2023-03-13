a19dea6c78be4fb6d68573f5426280a4978a811f
Noble-ch <123499650+Noble-ch@users.noreply.github.com>
2023-03-13 18:04:15 +0300

    Time: 0 ms (100.00%), Space: 40.9 MB (34.77%)

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=14 lang=java
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+class Solution {
+   public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+}
+// @lc code=end
+
