8315d5a2612aba21fccb7c03913ce06745e5dbcb
noble <123499650+Noble-ch@users.noreply.github.com>
2023-03-20 00:34:45 +0300

    Time: 0 ms (100.00%), Space: 42.4 MB (25.21%)

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,10 +10,12 @@
  */
 class Solution {
       public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-        
+              // create merged node with value 0 and a tail node innitially points to it
+        ListNode merged = new ListNode(0);
+        ListNode tail = merged;
+             // if list 1 and 2 are not empty and ...
         while (list1 != null && list2 != null) {
+            //list1 value is smaller than the value of list2
             if (list1.val <= list2.val) {
                 tail.next = list1;
                 list1 = list1.next;
@@ -31,6 +33,6 @@ class Solution {
             tail.next = list2;
         }
         
-        return dummy.next;
+        return merged.next;
     }
 }
